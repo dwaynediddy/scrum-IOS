94c23c7c853ed41f49d8b98c02ab4ace85ee47e3
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-23 21:13:31 +0800

    Initial Commit

---
+++ scrum/scrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumApp.swift
+//  scrum
+//
+//  Created by Dwayne on 23/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct scrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
