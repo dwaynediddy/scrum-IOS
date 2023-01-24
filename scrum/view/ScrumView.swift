7f5a7b360fd2a53eb6181649599962976f591f62
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-24 15:24:05 +0800

    hard coded scrum view added

---
+++ scrum/view/ScrumView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumView.swift
+//  scrum
+//
+//  Created by Dwayne on 24/1/2023.
+//
+
+import SwiftUI
+
+struct ScrumView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumView(scrums: DailyScrum.sampleData)
+    }
+}
