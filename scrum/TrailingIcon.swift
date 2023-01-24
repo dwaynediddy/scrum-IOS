b5b96c97b0cfa5e6e0995e6d8fdf2f1a34023268
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-24 13:09:07 +0800

    customising card view and making card view accessible

---
+++ scrum/TrailingIcon.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIcon.swift
+//  scrum
+//
+//  Created by Dwayne on 24/1/2023.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
