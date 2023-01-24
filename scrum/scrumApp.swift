2a9c6cfef1d018919961ca3b72d5bc424be13a5c
dwaynediddy <burrows.dwayne@icloud.com>
2023-01-24 15:40:02 +0800

    displaying the data in a list

--- scrum/scrumApp.swift
+++ scrum/scrumApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct scrumApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            ScrumView(scrums: DailyScrum.sampleData)
         }
     }
 }
