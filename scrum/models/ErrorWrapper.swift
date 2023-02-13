63d68211460e0ddbbe325956379adb570f187aa1
dwaynediddy <burrows.dwayne@icloud.com>
2023-02-13 13:55:29 +0100

    error view that displays an error to the user

---
+++ scrum/models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  scrum
+//
+//  Created by Dwayne on 13/2/2023.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+    
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
